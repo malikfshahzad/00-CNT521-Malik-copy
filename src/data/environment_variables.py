ef258154e50e9b83f57ca9e02b40adf71260f9e0
fsmpk <79743056+fsmpk@users.noreply.github.com>
2024-01-04 20:48:54 +0400

    Test .env file

---
+++ src/data/environment_variables.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_path = find_dotenv()
+
+load_dotenv(dotenv_path)
+
+API_KEY = os.getenv("API_KEY")
+print(API_KEY)
+
+password = os.getenv("pass")
+print(password)
